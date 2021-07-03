a8491f1b52fd6b6d1a4241ba086b35f9a387b50d
Josebias Neto <josebiasbsneto@gmail.com>
2021-07-03 11:14:12 -0300

    Create FullArticles Adapter

--- app/src/main/java/com/example/androidnews/FullArticle.kt
+++ app/src/main/java/com/example/androidnews/FullArticle.kt
@@ -2,10 +2,13 @@ package com.example.androidnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidnews.databinding.ActivityFullArticleBinding
 
 class FullArticle : AppCompatActivity() {
+    private lateinit var binding: ActivityFullArticleBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_full_article)
+        binding = ActivityFullArticleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

de66dc17cb8d818545246472e13eb442e3266980
Josebias Neto <josebiasbsneto@gmail.com>
2021-07-03 09:32:07 -0300

    Create Articles Adapter

--- app/src/main/java/com/example/androidnews/MainActivity.kt
+++ app/src/main/java/com/example/androidnews/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.androidnews
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.androidnews.databinding.ActivityFullArticleBinding
+import com.example.androidnews.databinding.ActivityMainBinding
 import model.Articles
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private inner class ArticleHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         fun bind(article: Articles){
             itemView.findViewById<TextView>(R.id.tv_name).text = article.name
@@ -37,7 +41,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val articles: MutableList<Articles> = ArrayList()
         for (a in 0..10){
@@ -51,5 +56,18 @@ class MainActivity : AppCompatActivity() {
         articleAdapter = ArticleAdapter(articles)
         findViewById<RecyclerView>(R.id.recyclerview).adapter = articleAdapter
         findViewById<RecyclerView>(R.id.recyclerview).layoutManager = LinearLayoutManager(applicationContext)
+
+        val recycler_articles = findViewById<RecyclerView>(R.id.recyclerview)
+        recycler_articles.layoutManager = GridLayoutManager(applicationContext, 3)
+
+
     }
+
+    private fun FullArticleFun(){
+        val intent = Intent(this, FullArticle::class.java)
+        startActivity(intent)
+    }
+
+
+
 }
--- app/src/main/java/model/Dados.kt
+++ app/src/main/java/model/Dados.kt
@@ -1,9 +1,53 @@
 package model
 
-class Articles(
+data class Articles(
 
     var name: String = "",
     var author: String = "",
     var title: String = ""
+
+)
+
+
+
+class ArticlesBuilder{
+
+    var nameArticle: String = ""
+    var authorArticle: String = ""
+    var titleArticle: String = ""
+
+    fun build(): Articles = Articles(nameArticle, authorArticle, titleArticle)
+
+}
+
+fun articles(block: ArticlesBuilder.() -> Unit): Articles = ArticlesBuilder().apply(block).build()
+
+fun addArticles(): MutableList<Articles> = mutableListOf(
+
+    articles {
+
+    },
+    articles {
+
+    },
+    articles {
+
+    },
+    articles {
+
+    },
+    articles {
+
+    },
+    articles {
+
+    },
+    articles {
+
+    },
+
+
+
+
 
 )

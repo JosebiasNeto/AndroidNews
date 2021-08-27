b8c033789ad87f92ecd5f23e91e4e652e79cd2d2
Josebias Neto <josebiasbsneto@gmail.com>
2021-08-27 18:18:09 -0300

    Fix the visibility problem

--- app/src/main/java/com/example/androidnews/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/androidnews/ui/view/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.androidnews.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -16,6 +15,7 @@ import com.example.androidnews.ui.ArticleAdapter
 import com.example.androidnews.ui.viewmodel.MainViewModel
 import com.example.androidnews.ui.viewmodel.ViewModelFactory
 import com.example.androidnews.utils.Status
+import kotlinx.coroutines.delay
 
 
 class MainActivity : AppCompatActivity() {
@@ -33,14 +33,6 @@ class MainActivity : AppCompatActivity() {
         setupUI()
         setupObservers()
 
-       // val recycler_articles = binding.recyclerview
-        ///recycler_articles.layoutManager = LinearLayoutManager(applicationContext)
-
-       // recycler_articles.addOnItemClickListener(object : OnItemClickListener {
-       //     override fun onItemClicked(position: Int, view: View) {
-                //openFullArticle(position)
-       //     }
-      //  })
 
         val pullToRefresh = binding.progressBar
         pullToRefresh.setOnRefreshListener {
@@ -75,18 +67,12 @@ class MainActivity : AppCompatActivity() {
             it?.let { resource ->
                 when (resource.status) {
                     Status.SUCCESS -> {
-                        binding.recyclerview.visibility = View.VISIBLE
-                        binding.progressBar.visibility = View.GONE
-                        resource.data?.let { articles -> retrieveList(articles as ArrayList<Article>) }
+                        resource.data?.articles?.let { it1 -> refreshAdapter(it1) }
                     }
                     Status.ERROR -> {
-                        binding.recyclerview.visibility = View.VISIBLE
-                        binding.progressBar.visibility = View.GONE
                         Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
                     }
                     Status.LOADING -> {
-                        binding.progressBar.visibility = View.VISIBLE
-                        binding.recyclerview.visibility = View.GONE
                     }
                 }
             }
@@ -94,12 +80,13 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    private fun retrieveList(articles: ArrayList<Article>){
+    private fun refreshAdapter(articles: ArrayList<Article>) {
         adapter.apply {
             addArticles(articles)
             notifyDataSetChanged()
         }
     }
+}
 
 
 
@@ -117,40 +104,5 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun getData() {
-
-        viewModel = ViewModelProvider(this,
-            MainViewModel.MainViewModelFactory(ArticlesAPI())
-        ).get(MainViewModel::class.java)
-        viewModel.articlesLiveData.observe(this, Observer { articles ->
-            binding.recyclerview.adapter = ArticleAdapter(arrayListOf())
-        })
-        viewModel.getArticles()
-
-
-
-        val retrofitClient = NetworkUtils()
-            .getRetrofitInstance("https://newsapi.org/v2/")
-
-        val endpoint = retrofitClient.create(ArticlesAPI::class.java)
-        val callback = endpoint.getArticles()
-
-        callback.enqueue(object : Callback<ArticlesResponse> {
-            override fun onFailure(call: Call<ArticlesResponse>, t: Throwable) {
-                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onResponse(
-                call: Call<ArticlesResponse>,
-                response: Response<ArticlesResponse>
-            ) {
-                if (response.body() != null) {
-                    articlesList = response.body()?.articles!!
-                    binding.recyclerview.adapter = ArticleAdapter(articlesList)
-                }
-            }
-        })*/
-
-}
-
+*/
 
